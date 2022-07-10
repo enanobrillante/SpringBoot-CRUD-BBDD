84f2a5a27a1f1b2f32e02d988c24ba4879b31528
enanobrillante <maurotissone@gmail.com>
2022-07-11 01:50:56 +0200

    Se agregó comentario

--- src/main/java/com/uploadfiles/entity/Usuario.java
+++ src/main/java/com/uploadfiles/entity/Usuario.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-
+//Clase usuario, prueba modificacion comentario para github
 @Entity
 @Table(name="upload_files")
 public class Usuario implements Serializable{
